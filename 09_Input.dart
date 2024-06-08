ac7603c86e2b6ee2ac91d773b5e64efd342550cc
AnisUrRehmanFarooq123456 <anees2217117@gmail.com>
2024-06-09 03:23:13 +0500

    Input Program added

---
+++ 09_Input.dart
@@ -0,0 +1,11 @@
+import "dart:io";
+void main () {
+  var a = stdin.readLineSync(); //With var you don't need to write ! in the end
+  print("Value Stored in a is $a and Datatpe is ${a.runtimeType}");
+
+  String b = stdin.readLineSync()!; //With String Datatype you need to write ! in the end
+  print("Value Stored in b is $b and Datatpe is ${b.runtimeType}");
+  
+  int c = int.parse(stdin.readLineSync()!); //With int Datatype you need to write int.parse
+  print("Value Stored in c is $c and Datatpe is ${c.runtimeType}");
+}
